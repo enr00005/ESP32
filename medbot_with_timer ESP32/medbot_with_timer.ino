5b94faf579aaed10cf9e1f961e44abd5a0696038
ENR <enr00005@gmail.com>
2026-02-17 13:58:52 +0530

    Modify WiFi credentials and HTML layout
    Updated WiFi credentials and improved HTML structure.

--- medbot_with_timer ESP32/medbot_with_timer.ino
+++ medbot_with_timer ESP32/medbot_with_timer.ino
@@ -2,8 +2,8 @@
 #include <WebServer.h>
 
 // ================= WIFI AP =================
-const char* ssid = "Evolve_4G";
-const char* password = "Evolve@7272";
+const char* ssid = "Enter your wifi name";
+const char* password = "Enter your wifi password";
 
 // ================= MOTOR PINS =================
 #define M1A 12
@@ -225,3 +225,4 @@ void loop(){
     buzzerActive = false;
   }
 }
+
